2b45dbaa5fd136f7f6f9c0a2ec873cb032b32fd9
yuzusim1~ <yuzusim@gmail.com>
2024-01-19 20:39:29 +0900

    Java에서 JDBC를 사용 DBMS에 연결

--- src/main/java/BankApp.java
+++ src/main/java/BankApp.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class BankApp {
+import db.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class BankApp {
+    public static void main(String[] args) {
+        Connection conn = DBConnection.getInstance();
+        // 이미 만들어진 버퍼를 활용
+        try {
+            String delete = "DELETE FROM account_tb WHERE number = ?";
+            PreparedStatement pstmt = conn.prepareStatement(delete);
+            pstmt.setInt(1, 1);
+
+            int num = pstmt.executeUpdate(); // flash
+            System.out.println(num);
+        } catch (Exception e) { //SQLException --> Exception 바꾸고 부모로 퉁침 멀티 트라이 캐치 안해도 됨
+            throw new RuntimeException(e);
+        }
+
+
+    }
 }
+
+
+
--- src/test/java/db/DBConnectionTest.java
+++ src/test/java/db/DBConnectionTest.java
@@ -1,2 +1,36 @@
-package db;public class DBConnectionTest {
+package db;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+// gradle 이라는 라이브러리러 세팅시 사용가능
+// junit 자바로 테스트 할 수 있는 도구
+
+public class DBConnectionTest {
+
+    // 리턴 타입을 못적는다. void 밖에 못적음
+    // 매개변수를 적을 수 없다.
+    // @Test 붙이면 메서드 별로 실행가능
+    @Test
+    public void getInstance_test(){
+        String username = "root";
+        String password ="1234";
+        String url = "jdbc:mariadb://localhost:3306/cosdb";
+        // 마리아디비 연결 프로토콜명을 찾아야 한다.
+        // DBMS 테이블까지 접근 가능하다.
+
+        // 프로토콜이 적용된 소켓
+        try {
+            Connection conn = DriverManager.getConnection(url, username, password);
+        } catch (Exception e) {
+            throw new RuntimeException(e); // JVM한테 던짐
+        }
+
+        // 유저 네임 부터 틀려봄 -> 디버깅 해봄
+        //
+
+    }
 }
